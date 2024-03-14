266a2771b176d5f76780aa45f8559d43bfbcf60a
Menna Shamloul <menna.shamloul92@gmail.com>
2024-03-14 11:10:44 +0000

    add comments function

---
+++ src/pages/comments/CommentEditForm.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import { axiosRes } from "../../api/axiosDefaults";
+
+import styles from "../../styles/CommentCreateEditForm.module.css";
+import CommentEditForm from "./CommentEditForm";
+
+function CommentEditForm(props) {
+  const { id, content, setShowEditForm, setComments } = props;
+  const [showEditForm, setShowEditForm] = useState(false);
+  const [formContent, setFormContent] = useState(content);
+
+  const handleChange = (event) => {
+    setFormContent(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      await axiosRes.put(`/comments/${id}/`, {
+        content: formContent.trim(),
+      });
+      setComments((prevComments) => ({
+        ...prevComments,
+        results: prevComments.results.map((comment) => {
+          return comment.id === id
+            ? {
+                ...comment,
+                content: formContent.trim(),
+                updated_at: "now",
+              }
+            : comment;
+        }),
+      }));
+      setShowEditForm(false);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <hr />
+      <Media>
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} />
+        </Link>
+        <Media.Body className="align-self-center ml-2">
+          <span className={styles.Owner}>{owner}</span>
+          <span className={styles.Date}>{updated_at}</span>
+          {showEditForm ? (
+            <CommentEditForm 
+                 id={id}
+                 profile_id={profile_id}
+                 content={content}
+                 profileImage={profile_image}
+                 setComments={setComments}
+                 setShowEditForm={setShowEditForm}
+            />
+          ) : (
+            <p>{content}</p>
+          )}
+        </Media.Body>
+        {is_owner && !showEditForm && (
+          <MoreDropdown
+            handleEdit={() => setShowEditForm(true)}
+            handleDelete={handleDelete}
+          />
+        )}
+      </Media>
+    </>
+  );
+}
+
+export default CommentEditForm;
